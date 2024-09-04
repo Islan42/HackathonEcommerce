1cc0a22ba3b6e93a6102d8bb31c499da7e16397c
Islan <islan12victhor@gmail.com>
2024-09-04 06:39:20 -0300

    feat: orders views first draft
    - Created the first draft for orders views

--- backend/orders/viewsets/views.py
+++ backend/orders/viewsets/views.py
@@ -2,17 +2,31 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 
+from django.shortcuts import get_object_or_404
+
+from orders.models.order import Order
+from orders.serializers.order_serializer import OrderSerializer
+from orders.models.order_item import OrderItem
+from orders.serializers.order_item_serializer import OrderItemSerializer
+from orders.models.payment import Payment
+from orders.serializers.payment_serializer import PaymentSerializer
+
 class OrdersList(APIView):
     def get(self, request):
-        data = {'Message': 'Hello There! OrdersList'}
-        return Response(data)
+        orders = Order.objects.all()
+        serializer = OrderSerializer(orders, many=True)
+        return Response(serializer.data)
 
 class OrdersCreate(APIView):
     def post(self, request):
-        data = {'Message': 'Hello There! OrdersCreate'}
-        return Response(data)
+        serializer = OrderSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class OrdersDelete(APIView):
     def delete(self, request, order_id):
-        data = {'Message': 'Hello There! OrdersDelete', 'Order': order_id}
-        return Response(data)
+        order = get_object_or_404(Order, pk=order_id)
+        order.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
